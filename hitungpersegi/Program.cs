7bbfed6dc122e21e25025b53e1a900e6c0b84efd
AskarKadir <askarkadir07@gmail.com>
2023-11-16 21:00:52 +0700

    Add project files.

---
+++ hitungpersegi/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Persegi
+{
+    public double Sisi { get; set; }
+
+    public double HitungLuas()
+    {
+        return Sisi * Sisi;
+    }
+
+    public double HitungKeliling()
+    {
+        return 4 * Sisi;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Test code
+        Console.WriteLine("Masukkan panjang sisi persegi:");
+        double panjangSisi = Convert.ToDouble(Console.ReadLine());
+
+        Persegi persegi = new Persegi { Sisi = panjangSisi };
+
+        double luas = persegi.HitungLuas();
+        double keliling = persegi.HitungKeliling();
+
+        Console.WriteLine($"Luas persegi: {luas}");
+        Console.WriteLine($"Keliling persegi: {keliling}");
+
+        Console.ReadLine(); // Biar console tidak langsung tertutup
+    }
+}
---
+++ hitungpersegiTests/PersegiTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[TestFixture]
+public class PersegiTests
+{
+    [TestCase(5, 25, 20)]
+    [TestCase(3, 9, 12)]
+    [TestCase(0, 0, 0)] // Test case for zero side length
+    public void HitungLuas_DiberikanSisiMengembalikanLuas(double sisi, double expectedLuas, double expectedKeliling)
+    {
+        Persegi persegi = new Persegi { Sisi = sisi };
+
+        double actualLuas = persegi.HitungLuas();
+
+        NUnit.Framework.Assert.AreEqual(expectedLuas, actualLuas);
+    }
+
+    [TestCase(5, 20, 20)]
+    [TestCase(3, 12, 12)]
+    [TestCase(0, 0, 0)] // Test case for zero side length
+    public void HitungKeliling_DiberikanSisiMengembalikanKeliling(double sisi, double expectedLuas, double expectedKeliling)
+    {
+        Persegi persegi = new Persegi { Sisi = sisi };
+
+        double actualKeliling = persegi.HitungKeliling();
+
+        NUnit.Framework.Assert.AreEqual(expectedKeliling, actualKeliling);
+    }
+}
